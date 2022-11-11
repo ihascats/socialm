eb658c5c4486d7ea5afaef2f695a8829f6315c18
ihascats <s.pavlovic.rs@gmail.com>
2022-11-11 18:12:24 +0100

    Add icons that will be used

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,7 +1,154 @@
-// import Icon from '@mdi/react';
+import Icon from '@mdi/react';
+import {
+  mdiTimelineClockOutline,
+  mdiBellOutline,
+  mdiBellBadgeOutline,
+  mdiAccountOutline,
+  mdiAccountAlertOutline,
+  mdiAccountMultiplePlusOutline,
+  mdiAccountMultipleRemoveOutline,
+  mdiThumbUpOutline,
+  mdiPencilOutline,
+  mdiDelete,
+  mdiDotsVertical,
+  mdiCommentOutline,
+} from '@mdi/js';
 
 export default function Icons() {
+  const timeline = (
+    <Icon
+      path={mdiTimelineClockOutline}
+      size={1.5}
+      horizontal
+      vertical
+      rotate={180}
+      color="inherit"
+    />
+  );
+  const notifications = (
+    <Icon
+      path={mdiBellOutline}
+      size={1.5}
+      horizontal
+      vertical
+      rotate={180}
+      color="inherit"
+    />
+  );
+  const notificationAlert = (
+    <Icon
+      path={mdiBellBadgeOutline}
+      size={1.5}
+      horizontal
+      vertical
+      rotate={180}
+      color="inherit"
+    />
+  );
+  const friendList = (
+    <Icon
+      path={mdiAccountOutline}
+      size={1.5}
+      horizontal
+      vertical
+      rotate={180}
+      color="inherit"
+    />
+  );
+  const friendRequest = (
+    <Icon
+      path={mdiAccountAlertOutline}
+      size={1.5}
+      horizontal
+      vertical
+      rotate={180}
+      color="inherit"
+    />
+  );
+  const acceptFriendRequest = (
+    <Icon
+      path={mdiAccountMultiplePlusOutline}
+      size={1.5}
+      horizontal
+      vertical
+      rotate={180}
+      color="inherit"
+    />
+  );
+  const declineFriendRequest = (
+    <Icon
+      path={mdiAccountMultipleRemoveOutline}
+      size={1.5}
+      horizontal
+      vertical
+      rotate={180}
+      color="inherit"
+    />
+  );
+  const like = (
+    <Icon
+      path={mdiThumbUpOutline}
+      size={1.5}
+      horizontal
+      vertical
+      rotate={180}
+      color="inherit"
+    />
+  );
+  const edit = (
+    <Icon
+      path={mdiPencilOutline}
+      size={1.5}
+      horizontal
+      vertical
+      rotate={180}
+      color="inherit"
+    />
+  );
+  const deleteSVG = (
+    <Icon
+      path={mdiDelete}
+      size={1.5}
+      horizontal
+      vertical
+      rotate={180}
+      color="inherit"
+    />
+  );
+  const moreOptions = (
+    <Icon
+      path={mdiDotsVertical}
+      size={1.5}
+      horizontal
+      vertical
+      rotate={180}
+      color="inherit"
+    />
+  );
+  const comment = (
+    <Icon
+      path={mdiCommentOutline}
+      size={1.5}
+      horizontal
+      vertical
+      rotate={180}
+      color="inherit"
+    />
+  );
+
   return {
     // ..icons
+    timeline,
+    notifications,
+    notificationAlert,
+    friendList,
+    friendRequest,
+    acceptFriendRequest,
+    declineFriendRequest,
+    like,
+    edit,
+    deleteSVG,
+    moreOptions,
+    comment,
   };
 }
