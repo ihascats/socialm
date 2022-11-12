5cdffeab1f2737c14ee7d62a54ccb2db8b5a7405
ihascats <s.pavlovic.rs@gmail.com>
2022-11-12 02:30:48 +0100

    Change Link depending on the page user is on

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,16 @@
 import { Link } from 'react-router-dom';
 import Icons from './Icons';
 
-export default function Nav() {
+export default function Nav({ timeline }) {
+  const showTimeline = false || timeline;
   const icons = Icons();
   return (
     <nav className="grid grid-cols-5 items-end justify-items-center sticky p-2 bottom-0 w-full h-fit border-t-4 bg-lime-300 fill-neutral-900 border-neutral-900 dark:bg-neutral-900 dark:fill-lime-300 dark:border-lime-300">
-      <Link to={`/user`}>{icons.profile}</Link>
+      {showTimeline ? (
+        <Link to={`/timeline`}>{icons.timeline}</Link>
+      ) : (
+        <Link to={`/user`}>{icons.profile}</Link>
+      )}
       <button>{icons.friendList}</button>
       <button>{icons.newPost}</button>
       <button>{icons.notifications}</button>
