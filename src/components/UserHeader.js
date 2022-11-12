c9740d75b48860d0f89e2b13bf3609b45caf3f6e
ihascats <s.pavlovic.rs@gmail.com>
2022-11-12 02:34:12 +0100

    Fix UserHeader color scheme not matching the rest

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,10 +1,10 @@
 export default function UserHeader({ userInformation }) {
   return (
-    <div className="flex gap-2 items-end p-2 w-full dark:bg-neutral-800 dark:text-neutral-50">
+    <div className="flex gap-2 items-end p-2 w-full dark:bg-neutral-900 dark:text-neutral-50">
       <img
         alt=""
         src={userInformation.profile_picture}
-        className="min-w-fit h-16 rounded-full border-2 border-neutral-900 dark:border-orange-300"
+        className="min-w-fit h-16 rounded-full border-2 border-neutral-900 dark:border-lime-300"
       ></img>
       <h1 className="font-mono font-bold text-2xl">
         {userInformation.username}
