e4512b95b00ade6a687c437af475fb294a992659
ihascats <s.pavlovic.rs@gmail.com>
2022-11-15 00:04:45 +0100

    Fix MOMenu being off screen

--- src/components/MoreOptionsMenu.js
+++ src/components/MoreOptionsMenu.js
@@ -4,7 +4,7 @@ export default function MoreOptionsMenu() {
   const icons = Icons();
 
   return (
-    <div className="optionsMenu fixed mt-3 grid right-10 bg-lime-200 pl-2 pr-14 py-1 justify-items-start font-mono rounded-lg">
+    <div className="optionsMenu absolute mt-3 grid right-10 bg-lime-200 pl-2 pr-14 py-1 justify-items-start font-mono rounded-lg">
       <button className="flex gap-2">
         {icons.edit}
         <p>Edit</p>
