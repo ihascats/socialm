1377736d0e4f41df2d622115cd0c78648a751d4e
ihascats <s.pavlovic.rs@gmail.com>
2022-11-26 20:20:43 +0100

    Get post image if its a url

--- src/fetch_requests/img.fetch.js
+++ src/fetch_requests/img.fetch.js
@@ -11,3 +11,17 @@ exports.fetchImage = async function (userInformation, setUserInformation) {
       setUserInformation(clone);
     });
 };
+
+exports.fetchPostImage = async function (postInformation, setPostInformation) {
+  if (!postInformation) return;
+  fetch(`${process.env.REACT_APP_APILINK}/post/img/${postInformation._id}`)
+    //                         vvvv
+    .then((response) => response.blob())
+    .then((imageBlob) => {
+      // Then create a local URL for that image and print it
+      const imageObjectURL = URL.createObjectURL(imageBlob);
+      const clone = structuredClone(postInformation);
+      clone.image = imageObjectURL;
+      setPostInformation(clone);
+    });
+};
