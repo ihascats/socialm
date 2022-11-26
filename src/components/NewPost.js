a99a92d6302611b7d85fbc99b767659856b1266e
ihascats <s.pavlovic.rs@gmail.com>
2022-11-26 20:21:54 +0100

    Add fields for submitting post image

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,4 +1,5 @@
 import { useRef } from 'react';
+import Icons from './Icons';
 import NewPostButtons from './mini-components/buttons/NewPostButtons';
 
 export default function NewPost({
@@ -7,6 +8,9 @@ export default function NewPost({
   setUserPosts,
 }) {
   const textArea = useRef();
+  const imageFile = useRef();
+  const imageUrl = useRef();
+  const icons = Icons();
 
   return (
     <div className="fixed top-0 min-h-screen-nav w-full backdrop-brightness-50 backdrop-blur-sm text-neutral-200">
@@ -19,11 +23,33 @@ export default function NewPost({
           placeholder="What's happening?"
           className="w-full bg-transparent border-b-2 border-l-2 border-blue-600 resize-none h-20 outline-offset-4 p-2"
         ></textarea>
+        <div className="flex gap-1 text-blue-300 fill-blue-200">
+          <button
+            onClick={(event) => {
+              event.preventDefault();
+              imageFile.current.click();
+            }}
+            className="bg-orange-200/20 rounded-lg"
+          >
+            {icons.addImage}
+          </button>
+          <input ref={imageFile} type="file" hidden></input>
+          <label htmlFor="url" className="flex gap-1 font-mono">
+            url:
+            <input
+              ref={imageUrl}
+              id="url"
+              className="bg-blue-300/30 rounded-lg"
+            ></input>
+          </label>
+        </div>
         <NewPostButtons
           setNewPostVisible={setNewPostVisible}
           setTimeline={setTimeline}
           setUserPosts={setUserPosts}
           textArea={textArea}
+          imageFile={imageFile}
+          imageUrl={imageUrl}
         />
       </div>
     </div>
