7010f1ccde253cf8f075be87ea7da8bbf7897031
ihascats <s.pavlovic.rs@gmail.com>
2022-11-28 22:55:28 +0100

    Fetch chat messages

---
+++ src/fetch_requests/chat.fetch.js
@@ -0,0 +1,12 @@
+exports.fetchChat = async function () {
+  const link = `${process.env.REACT_APP_APILINK}/chat`;
+  const response = await fetch(link, {
+    mode: 'cors',
+  });
+  if (response.status === 200) {
+    const json = await response.json(); //extract JSON from the http response
+    return { chat: json, response };
+  } else {
+    return { response };
+  }
+};
