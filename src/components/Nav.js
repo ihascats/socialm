35bab10ea001e0cf45a6645823c84a81af158d56
ihascats <s.pavlovic.rs@gmail.com>
2022-11-29 15:26:25 +0100

    Add logout, google & guest login to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import Icons from './Icons';
 import NewComment from './NewComment';
 import NewPost from './NewPost';
@@ -16,8 +16,12 @@ export default function Nav({
   const showTimeline = false || timeline;
   const icons = Icons();
 
-  return (
-    <nav className="grid grid-cols-5 items-end justify-items-center sticky p-2 bottom-0 w-full h-fit border-t-4 bg-lime-300 fill-neutral-900 border-neutral-900 dark:bg-neutral-900 dark:fill-lime-300 dark:border-lime-300">
+  const navigate = useNavigate();
+
+  return localStorage.Authorization ? (
+    <nav
+      className={`grid grid-cols-6 items-end justify-items-center sticky p-2 bottom-0 w-full h-fit border-t-4 bg-lime-300 fill-neutral-900 border-neutral-900 dark:bg-neutral-900 dark:fill-lime-300 dark:border-lime-300`}
+    >
       {showTimeline ? (
         <Link to={`/timeline`}>{icons.timeline}</Link>
       ) : (
@@ -50,6 +54,14 @@ export default function Nav({
           setUserPosts={setUserPosts}
         />
       ) : null}
+      <button
+        onClick={() => {
+          localStorage.removeItem('Authorization');
+          navigate(`${process.env.PUBLIC_URL}/`, { replace: true });
+        }}
+      >
+        {icons.logout}
+      </button>
       {newCommentVisible ? (
         <NewComment
           setNewCommentVisible={setNewCommentVisible}
@@ -59,6 +71,31 @@ export default function Nav({
           setPostInformation={setPostInformation}
         />
       ) : null}
+    </nav>
+  ) : (
+    <nav
+      className={`grid grid-cols-2 items-end justify-items-center sticky p-2 bottom-0 w-full h-fit border-t-4 bg-lime-300 fill-neutral-900 border-neutral-900 dark:bg-neutral-900 dark:fill-lime-300 dark:border-lime-300`}
+    >
+      <a
+        href={`${process.env.REACT_APP_APILINK}/auth/google`}
+        className="h-full flex gap-4 text-lg items-center"
+      >
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/800px-Google_%22G%22_Logo.svg.png"
+          alt="google logo"
+          className="h-12"
+        />
+        Sign In
+      </a>
+      <button
+        onClick={() => {
+          localStorage.setItem('Authorization', process.env.REACT_APP_GUEST);
+          navigate(`${process.env.PUBLIC_URL}/`, { replace: true });
+        }}
+        className="h-full text-lg items-center"
+      >
+        Sign In as Guest
+      </button>
     </nav>
   );
 }
