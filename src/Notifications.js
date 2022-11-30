a4c915fc1b6cf57356604f920f4d0ee0fdcb6150
ihascats <s.pavlovic.rs@gmail.com>
2022-11-30 15:19:42 +0100

    Create a button that clears out notifications

--- src/Notifications.js
+++ src/Notifications.js
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react';
 import Nav from './components/Nav';
 import NotificationCard from './components/NotificationCard';
-import { fetchNotifications } from './fetch_requests/notifications.fetch';
+import {
+  fetchNotifications,
+  fetchPutClearNotifications,
+} from './fetch_requests/notifications.fetch';
 
 export default function Notifications() {
   const [allNotifications, setAllNotifications] = useState();
@@ -19,9 +22,28 @@ export default function Notifications() {
     );
   }, []);
 
+  function clearNotifications() {
+    fetchPutClearNotifications().then(
+      function (value) {
+        if (value.response.status === 200) {
+          setAllNotifications(value.notifications);
+        }
+      },
+      function (error) {
+        console.log(error);
+      },
+    );
+  }
+
   return (
     <div>
       <div className="bg-gradient-to-b from-violet-800 to-cyan-600 h-screen-nav min-h-screen-nav max-h-screen text-neutral-300 overflow-auto hide-scroll">
+        <button
+          className="w-full bg-red-500 p-2 sticky top-0"
+          onClick={clearNotifications}
+        >
+          Clear notifications
+        </button>
         <ul className="min-h-screen-nav overflow-auto hide-scroll scroll-smooth">
           {allNotifications
             ? allNotifications.unread_notifications
