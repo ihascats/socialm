338a5902aea1c95bdb8e02b60f8332c68ab142f6
ihascats <s.pavlovic.rs@gmail.com>
2022-11-30 01:33:52 +0100

    Add a dark/light theme switch

--- src/components/mini-components/AdditionalNavOptions.js
+++ src/components/mini-components/AdditionalNavOptions.js
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Icons from '../Icons';
 
 export default function AdditionalNavOptions() {
   const navigate = useNavigate();
   const icons = Icons();
+  const [theme, setTheme] = useState(false);
 
   return (
     <div className="fixed bottom-16 right-0 border-l-4 border-t-4 border-neutral-900 bg-lime-400 w-40 z-50">
@@ -21,6 +23,15 @@ export default function AdditionalNavOptions() {
         className="flex items-center px-2 py-1 hover:bg-neutral-900/50 w-full"
       >
         {icons.logout} Sign Out
+      </button>
+      <button
+        onClick={() => {
+          document.body.classList.toggle('dark');
+          setTheme((prev) => !prev);
+        }}
+        className="flex items-center px-2 py-1 hover:bg-neutral-900/50 w-full"
+      >
+        {theme ? icons.dark : icons.light} Theme
       </button>
     </div>
   );
