67711b2416f05284fb8719376e56123d3da3fec5
ihascats <s.pavlovic.rs@gmail.com>
2022-11-30 15:19:15 +0100

    Add a fetch request that clears out notifications

--- src/fetch_requests/notifications.fetch.js
+++ src/fetch_requests/notifications.fetch.js
@@ -13,3 +13,20 @@ exports.fetchNotifications = async function () {
     return { response };
   }
 };
+
+exports.fetchPutClearNotifications = async function () {
+  const link = `${process.env.REACT_APP_APILINK}/notifications`;
+  const response = await fetch(link, {
+    mode: 'cors',
+    method: 'PUT',
+    headers: new Headers({
+      Authorization: localStorage.Authorization,
+    }),
+  });
+  if (response.status === 200) {
+    const json = await response.json(); //extract JSON from the http response
+    return { notifications: json, response };
+  } else {
+    return { response };
+  }
+};
