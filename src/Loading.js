f7d53c1006f453551caa0e195682613c9076b674
ihascats <s.pavlovic.rs@gmail.com>
2023-03-08 00:53:25 +0100

    Make user aware that loading can take few minutes

--- src/Loading.js
+++ src/Loading.js
@@ -34,8 +34,16 @@ export default function Loading() {
 
   const icons = Icons();
   return (
-    <div className="w-screen h-screen flex justify-center items-center dark:fill-neutral-50">
+    <div className="w-screen h-screen flex flex-col dark:text-white justify-center items-center dark:fill-neutral-50">
       {icons.loading}
+      <p className="text-lg tracking-wider">
+        establishing a connection with the server
+      </p>
+      <div className="font-mono text-xs flex">
+        (This can take up to a several minutes)
+        <p className="loader__dot">.</p>
+        <p className="loader__dot">.</p>
+      </div>
     </div>
   );
 }
